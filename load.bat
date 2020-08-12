77d3e02b3b15e3f95fc4c6e60e13424ca6f1fa43
kodedile <69500228+kodedile@users.noreply.github.com>
2020-08-12 11:34:25 -0400

    feat: load from arg, prompt overwrite, no echo

--- load.bat
+++ load.bat
@@ -1,12 +1,17 @@
-::@ECHO OFF
+@ECHO OFF
 SETLOCAL
 :: directory for Kura5 data files
 SET DataFolder=%cd%\..\Kura5_Data
 :: directory for storing save files
 SET SaveFolder=%cd%\Save_Files
-:: prompt user to specify a save file
-SET /p file= Enter Name of a Save File: 
-
+:: IF a file was not opened with this script
+IF [%1]==[] (
+	:: prompt user to specify a save file
+	SET /p file= Enter Name of a Save File: 
+) ELSE (
+	:: use the file that was opened with (dropped on) this script
+	SET file=%1
+)
 :: parse the filepath
 FOR /f "delims=" %%i IN ("%file%") DO (
 	SET FileName=%%~ni
@@ -14,4 +19,4 @@ FOR /f "delims=" %%i IN ("%file%") DO (
 :: create the data folder in case it doesn't exist yet
 MKDIR "%DataFolder%"
 :: copy save file to data folder
-COPY "%SaveFolder%\%FileName%.bok" "%DataFolder%\save.bok"
+COPY /-Y "%SaveFolder%\%FileName%.bok" "%DataFolder%\save.bok"
