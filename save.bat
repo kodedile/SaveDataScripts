fe76e482858505ca32b3a226c6b4814c28fdaf2b
kodedile <69500228+kodedile@users.noreply.github.com>
2020-08-12 11:35:01 -0400

    feat: save to arg, prompt overwrite

--- save.bat
+++ save.bat
@@ -4,9 +4,19 @@ SETLOCAL
 SET DataFolder=%cd%\..\Kura5_Data
 :: directory for storing save files
 SET SaveFolder=%cd%\Save_Files
-:: prompt user to name the save file
-SET /p FileName= Enter Name for Save File: 
+:: IF a file was not passed to this script
+IF [%1]==[] (
+	:: prompt user to specify a save file
+	SET /p file= Enter Name for Save File: 
+) ELSE (
+	:: use the file that was provided to this script
+	SET file=%1
+)
+:: parse the filepath
+FOR /f "delims=" %%i IN ("%file%") DO (
+	SET FileName=%%~ni
+)
 :: create the save folder in case it doesn't exist yet
 MKDIR "%SaveFolder%"
 :: copy data file to save folder
-COPY "%DataFolder%\save.bok" "%SaveFolder%\%FileName%.bok"
+COPY /-Y "%DataFolder%\save.bok" "%SaveFolder%\%FileName%.bok"
